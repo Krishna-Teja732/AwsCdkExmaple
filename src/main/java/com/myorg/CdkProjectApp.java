b7c9db29d6983ddd4dc4bc5ab5f0f36a2e88d054
Krishna Teja <bkritej@amazon.con>
2023-03-20 01:27:27 +0530

    cdk pipeline

--- src/main/java/com/myorg/CdkProjectApp.java
+++ src/main/java/com/myorg/CdkProjectApp.java
@@ -4,7 +4,6 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-
 public class CdkProjectApp {
     public static void main(final String[] args) {
         App app = new App();
@@ -15,6 +14,13 @@ public class CdkProjectApp {
                         .region("us-west-2")
                         .build())
                 .build());
+
+        new CdkPipeline(app, "CdkPipeline", StackProps.builder()
+                .env(Environment.builder()
+                        .account("387030945958")
+                        .region("us-west-2")
+                        .build())
+                    .build());
 
         app.synth();
     }
